97016bf16817f15c30175100801e520c974f9279
ErikCasas <santicasas667@gmail.com>
2024-05-19 12:09:03 -0500

    rename folder page for pages and remove app.test.tsx unnecessary

--- src/App.jsx
+++ src/App.jsx
@@ -1,21 +1,21 @@
-import { Route, BrowserRouter as Router, Routes } from "react-router-dom";
-import { Menu } from "./components/Menu";
-import { HomePage } from "./page/HomePage";
-import { TodoList } from "./page/TodoList";
-import { AboutUs } from "./page/AboutUs";
-import { TaskDetail } from "./page/TaskDetail";
+import { Route, BrowserRouter as Router, Routes } from 'react-router-dom'
+import { Menu } from './components/Menu'
+import { HomePage } from './pages/HomePage'
+import { TodoList } from './pages/TodoList'
+import { AboutUs } from './pages/AboutUs'
+import { TaskDetail } from './pages/TaskDetail'
 
 export function App() {
-  return (
-    <Router>
-      <Menu />
+	return (
+		<Router>
+			<Menu />
 
-      <Routes>
-        <Route path="/" exact element={<HomePage />} />
-        <Route path="/todo-list" element={<TodoList />} />
-        <Route path="/about-us" element={<AboutUs />} />
-        <Route path="/task/:taskId" element={<TaskDetail />} />
-      </Routes>
-    </Router>
-  );
+			<Routes>
+				<Route path='/' exact element={<HomePage />} />
+				<Route path='/todo-list' element={<TodoList />} />
+				<Route path='/about-us' element={<AboutUs />} />
+				<Route path='/task/:taskId' element={<TaskDetail />} />
+			</Routes>
+		</Router>
+	)
 }
